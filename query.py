6302474792a08012250621c03cd6d8aea6c38688
Francesco Torregrossa <d.ftorregrossa@gmail.com>
2020-07-07 19:52:07 +0200

    fix federated query for distance

--- query.py
+++ query.py
@@ -36,14 +36,14 @@ def get_station_for(latitude, longitude):
     PREFIX sk: <http://www.w3.org/2004/02/skos/core#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
 
-    select ?internal_stat ?dist
+    select ?internal_stat bif:st_distance(bif:st_point(""" + str(latitude) + ", " + str(longitude) + """), bif:st_point(?s_lat, ?s_long)) as ?dist
     where {
         service <https://semantic.eea.europa.eu/sparql> {
-            select ?stat bif:sqrt((""" + str(latitude) + """ - ?latitude) * (""" + str(latitude) + """ - ?latitude) + (""" + str(longitude) + """ - ?longitude) * (""" + str(longitude) + """ - ?longitude)) as ?dist
+            select ?stat ?latitude ?longitude
             where {
                 ?stat rdf:type airbase:Station ;
-                      geo:lat ?s_lat ;
-                      geo:long ?s_long ;
+                      geo:lat ?latitude ;
+                      geo:long ?longitude ;
                       airbase:country ?nation .
                 ?nation sk:notation ?nation_code .
                 filter(?nation_code='IT') .
